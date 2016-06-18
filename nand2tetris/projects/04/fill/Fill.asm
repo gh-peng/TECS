f47cbb5f440489adf2e66027a94f18ee159539df
彭光辉 <gh.peng@qq.com>
2016-06-18 12:54:42 +0800

    04

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -8,4 +8,40 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+  // Put your code here.
+  @0
+  M=0
+  (check_kbd)
+  @KBD
+  D=M
+  @clean
+  D                             ;JEQ
+  @0
+  D=M
+  @8192
+  D=D-A
+  @check_kbd
+  D                             ;JEQ
+  @0
+  D=M
+  @SCREEN
+  A=D+A
+  D=0
+  D=!D
+  M=D
+  @0
+  M=M+1
+  @check_kbd
+  0                             ;JMP
+  (clean)
+  @0
+  D=M
+  @check_kbd
+  D                             ;JLT
+  @0
+  MD=M-1
+  @SCREEN
+  A=D+A
+  M=0
+  @check_kbd
+  0                             ;JMP
--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -6,4 +6,22 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+  // Put your code here.
+  @2
+  M=0
+  (loop)
+  @1
+  D=M
+  @end
+  D                             ;JEQ
+  @0
+  D=M
+  @2
+  M=D+M
+  @1
+  M=M-1
+  @loop
+  0                             ;JMP
+  (end)
+  @end
+  0                             ;JMP
